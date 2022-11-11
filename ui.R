ccecf606471b4d15a3c3f7aa8680010154f83561
Shuchen0920 <117210836+Shuchen0920@users.noreply.github.com>
2022-11-11 16:49:15 +0800

    about tab edited

--- ui.R
+++ ui.R
@@ -189,7 +189,7 @@ shinyUI(navbarPage(title = "IntelliShare",
                                                align = "center"
                                       ),
                                       
-                                      br()
+                                      hr()
                                       
                             ),
                             
