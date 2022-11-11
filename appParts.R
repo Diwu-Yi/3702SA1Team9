f48c4128cd199f58e043194ee8e647e98a9650b6
Shuchen0920 <117210836+Shuchen0920@users.noreply.github.com>
2022-11-11 21:29:36 +0800

    ui cleaned

--- appParts.R
+++ appParts.R
@@ -8,130 +8,20 @@ neighborhoodDescription <- function() {
             p("Interested to find out nearest available carparks? Use this map-based browser to checkout the availability of carparks near your place."),
             p("Use the Longitude and Latitude Inputs to center the map at your place. Don't your coordinates ? Simply google 'my map coordinates' !"),
             p("Use the slider to show only k available carparks around to avoid conflicts with others."),
-            fluidRow(
-                column(12,
+            sidebarLayout(
+              sidebarPanel(
                        numericInput("lon", label = h3("Enter longitude of your location"), value = 34.79399507628829),
                        numericInput("lat", label = h3("Enter latitude of your location"), value = 32.08886188641638),
                        sliderInput("topK",label = h3("Show nearest k carparks"),
                                    min = 50, 
                                    max = 500, value = 200),
                        radioButtons(inputId = "mapFormat",label="View Format",
-                                    choices = c("Point","Heatmap","Cluster")),
+                                    choices = c("Point","Heatmap","Cluster"))),
+              mainPanel(
                        leafletOutput("map", height = 600)
-                       ),
-                hidden(column(5, class = "hood-info", id = "reactiveOutput1",
-                       h1(textOutput("hood"), class = "heading"),
-                       htmlOutput("hoodInfo")
-                       ))
-            ),
-            hidden(
-                div(class = "kpi-group",
-                    fluidRow(style = "margin-top: 10px;",
-                                    id = "reactiveOutput2a",
-                             column(3,
-                                    div(plotlyOutput("donut", height = "100%"), align = "center"),
-                                    h3("Intelligentsia Score", class = "kpi-name")
-                             ),
-                             column(3,
-                                    h2(textOutput("kpi1"), class = "kpi"),
-                                    h3("Median Home Value", class = "kpi-name")
-                             ),
-                             column(3,
-                                    h2(textOutput("kpi2"), class = "kpi"),
-                                    h3("Median Housing Cost", class = "kpi-name")
-                             ),
-                             column(3,
-                                    h2(textOutput("kpi3"), class = "kpi"),
-                                    h3("Year built (median)", class = "kpi-name")
-                             )
-                    ),
-                    fluidRow(id = "reactiveOutput2b",
-                             column(3,
-                                    h2(textOutput("kpi4"), class = "kpi"),
-                                    HTML("<h3 class='kpi-name'>Avg. Yelp Rating<sup>1</sup></h3>")
-                             ),
-                             column(3,
-                                    h2(textOutput("kpi5"), class = "kpi"),
-                                    HTML("<h3 class='kpi-name'>Walkability Index<sup>2</sup></h3>")
-                             ),
-                             column(3,
-                                    h2(textOutput("kpi6"), class = "kpi"),
-                                    HTML("<h3 class='kpi-name'># of Trees<sup>3</sup></h3>")
-                             ),
-                             column(3,
-                                    h2(textOutput("kpi7"), class = "kpi"),
-                                    HTML("<h3 class='kpi-name'>% Share of Taxicab Rides at Night<sup>4</sup></h3>")
-                             )
-                    ),
-                    fluidRow(id = "reactiveOutput2c",
-                             column(3,
-                                    h2(textOutput("kpi8"), class = "kpi"),
-                                    h3("% College Education", class = "kpi-name")
-                             ),
-                             column(3,
-                                    h2(textOutput("kpi9"), class = "kpi"),
-                                    h3("Median Age", class = "kpi-name")
-                             ),
-                             column(3,
-                                    h2(textOutput("kpi10"), class = "kpi"),
-                                    h3("% Family Households", class = "kpi-name")
-                             ),
-                             column(3,
-                                    h2(textOutput("kpi11"), class = "kpi"),
-                                    h3("% Crime", class = "kpi-name")
-                             )
-                    ),
-                    fluidRow(column(12,tags$small("Sources: U.S. Census Bureau, 1. Yelp, 2. BEH, 3. NYC Open Data, 4. NYC TLC")))
-                ),
-                hr(),
-                fluidRow(id = "reactiveOutput3",
-                    column(12,
-                           h2("Development of real estate prices"),
-                           div(plotlyOutput("zillow1", width = "100%"), align = "center", 
-                               class = "chart", width = "100%")
-                           )
-                ),
-                hr(),
-                fluidRow(id = "reactiveOutput4",
-                         column(12,
-                                h2("Yelp reviews over time"),
-                                div(plotlyOutput("yelp", height = "420px"), align = "center",
-                                    class = "chart")
-                                )
-                ),
-                hr(),
-                fluidRow(id = "reactiveOutput4a1",
-                         column(12,h2("Online Activities"))),
-                fluidRow(id = "reactiveOutput4a",
-                         column(6,
-                                h3("Google Search Trends"),
-                                div(plotlyOutput("google"), align = "center",
-                                    class = "chart")
-                                ),
-                         column(6,
-                                h3("Wikipedia Edits"),
-                                div(plotlyOutput("wiki"), align = "center",
-                                    class = "chart")
-                         )
-                ),
-                hr(),
-                fluidRow(id = "reactiveOutput5",
-                         column(12,
-                                h2("Taxi trips over time"),
-                                div(plotlyOutput("taxi"), align = "center", 
-                                    class = "chart")
-                                )
-                ),
-                hr(),
-                fluidRow(id = "reactiveOutput6",
-                         column(12,
-                                h2("Properties currently for sale on Zillow"),
-                                div(htmlOutput("propertiesForSale"),
-                                    class = "property-card-container"))
-                         )
-            )
+                       )
         )
-    )
+    ))
 }
 
 
